cd56b920d77752231e894cf7bb4be79c6e80d211
Lidor Yosef <lidor323@gmail.com>
2016-04-20 13:14:35 +0300

    Added more Image scenes. now containing stars example and last scene followers that have colored trails. added trail GearVR support but currently there some kind of security error assumed to be connected the the osig file

---
+++ Assets/scripts/BasicMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        self = transform;
+        yAxis = self.position.y;
+	}
+    public float radius = 1;
+    Transform self;
+    float yAxis = 0;
+	// Update is called once per frame
+	void Update () {
+        self.position = new Vector3(radius * Mathf.Sin(Time.time),yAxis, radius * Mathf.Cos(Time.time));
+	}
+}

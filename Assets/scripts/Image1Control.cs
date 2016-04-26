f86f35ecb631a80901491d3bef4bc027d90c66de
Lidor Yosef <lidor323@gmail.com>
2016-04-26 23:39:01 +0300

    Connected 1st and 2nd scenes added transitions animatons on the first scene made the order pre set to avoid the black line glitches

---
+++ Assets/scripts/Image1Control.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Image1Control : MonoBehaviour {
+    [System.Serializable]
+    public struct action
+    {
+        public Transform obj;
+        public float delay;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public action[] actions;
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButton("Submit"))
+        {
+            Debug.Log("Presentation started");
+            for(int i = 0;i<actions.Length;i++)
+            {
+                StartCoroutine(startAction(actions[i]));
+            }
+        }
+	
+	}
+
+    IEnumerator startAction(action act)
+    {
+        yield return new WaitForSeconds(act.delay);
+        act.obj.GetComponent<IEnable>().Enable(null);
+    }
+}
---
+++ Assets/scripts/StartAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartAnimation : MonoBehaviour, IEnable {
+
+    public Animator anim;
+    public string booleanName;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool(booleanName, true);
+
+	}
+	public void Enable(object data)
+    {
+        this.enabled = true;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
